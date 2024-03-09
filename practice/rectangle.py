3dc6a20d614e2831e76c100f5e8c3bbf0a7f9d41
AlexanderSeverovNorway <154597159+AlexanderSeverovNorway@users.noreply.github.com>
2024-03-09 11:34:54 +0500

    Площадь фигуры с полиморфизмом

--- practice/rectangle.py
+++ practice/rectangle.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Rectangle:
     def __init__(self, a, b):
         self.a = a
@@ -11,3 +14,12 @@ class Square:
 
     def get_area_square(self):
         return self.a ** 2
+
+
+class Round:
+    def __init__(self, R):
+        self.R = R
+
+    def get_area_round(self):
+        math.pi = 3.14
+        return math.pi * (self.R ** 2)
--- practice/rectangle_2.py
+++ practice/rectangle_2.py
@@ -1,4 +1,4 @@
-from rectangle import Rectangle, Square
+from rectangle import Rectangle, Square, Round
 rect_1 = Rectangle(3, 4)
 rect_2 = Rectangle(12, 5)
 print(rect_1.get_area())
@@ -8,6 +8,11 @@ square_1 = Square(5)
 square_2 = Square(10)
 print(square_1.get_area_square())
 print(square_2.get_area_square())
+
+round_1 = Round(5)
+round_2 = Round(15)
+print(round_1.get_area_round())
+print(round_2.get_area_round())
 
 
 figures = [rect_1, rect_2, square_1, square_2]

bccbc2a8e7c8357792823f1e6dd3222a582eeada
AlexanderSeverovNorway <154597159+AlexanderSeverovNorway@users.noreply.github.com>
2024-03-15 13:22:31 +0500

    Проверка корректности расстановки скобок в типе string через реализацию стека

--- practice/Корректность_расстановки_скобок_string.py
+++ practice/Корректность_расстановки_скобок_string.py
@@ -1,16 +1,15 @@
-def par_checker(string):
-    stack = []  # инициализируем стек
+pars = {")": "(", "]": "["}
 
-    for s in string:  # читаем строку посимвольно
-        if s == "(":  # если открывающая скобка,
-            stack.append(s)  # добавляем её в стек
-        elif s == ")":
-            # если встретилась закрывающая скобка, то проверяем
-            # пуст ли стек и является ли верхний элемент — открывающей скобкой
-            if len(stack) > 0 and stack[-1] == "(":
-                stack.pop()  # удаляем из стека
-            else:  # иначе завершаем функцию с False
+
+def par_checker_mod(string):
+    stack = []
+
+    for s in string:
+        if s in "([":
+            stack.append(s)
+        elif s in ")]":
+            if len(stack) > 0 and stack[-1] == pars[s]:
+                stack.pop()
+            else:
                 return False
-    # если стек пустой, то незакрытых скобок не осталось
-    # значит, возвращаем True, иначе — False
     return len(stack) == 0
